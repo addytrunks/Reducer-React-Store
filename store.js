e20f64f96ed7c13c35078375171cdad175622cad
Adhithya Srivatsan <65851764+addytrunks@users.noreply.github.com>
2020-12-02 13:45:28 +0530

    Create store.js
    Reducer Store

---
+++ store.js
@@ -0,0 +1,23 @@
+// All the reducers , middlewares and stuff goes here.
+
+import { combineReducers,createStore,applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+//You wanna put all the reducers here.
+const reducer = combineReducers({
+    // Pass your reducers here.
+})
+
+//Contains thing you want to fire off once redux loads.
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+    )
+
+export default store
